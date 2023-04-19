c7261c54b735fc2c0d8191f636c5181e04cc1eee
LeonardoFilesi <leonardo.filesi@gmail.com>
2023-04-19 17:55:20 +0200

    feat: :sparkles: Adds: basic App.vue, importing files

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,38 @@
 <script>
+import AppHeader from "./components/AppHeader.vue";
+import AppSearchbar from "./components/AppSearchbar.vue";
+import AppMain from "./components/AppMain.vue";
+import AppCard from "./components/AppCard.vue";
+import AppFooter from "./components/AppFooter.vue";
+import axios from "axios";
 
+export default {
+  components: {
+    AppHeader,
+    AppSearchbar,
+    AppMain,
+    AppCard,
+    AppFooter,
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
 </script>
 
 <template>
-
+<AppHeader />
+<AppSearchbar />
+<AppMain />
+<AppCard />
+<AppFooter />
 </template>
 
 <style scoped lang="scss">
-
+@use "./style/general.scss"
 </style>
